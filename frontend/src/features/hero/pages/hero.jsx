488be0e37535fa647ffec697e64e0d8d0333ffab
Joven <nevojnarim@gmail.com>
2026-03-23 15:44:18 +0800

    Support copy email on hero section

--- frontend/src/features/hero/pages/hero.jsx
+++ frontend/src/features/hero/pages/hero.jsx
@@ -17,6 +17,17 @@ const Hero = () => {
     return () => clearTimeout(timeout); // cleanup
   }, []);
 
+  const [copyEmail, setCopyEmail] = useState(false);
+  // turn back to false after triggered to true
+  useEffect(() => {
+    if (copyEmail) {
+      const timer = setTimeout(() => {
+        setCopyEmail(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [copyEmail]);
+
   return (
     <div className="flex w-full md:flex-row md:gap-0 gap-10 flex-col-reverse justify-between items-center">
       <Motion
@@ -24,7 +35,9 @@ const Hero = () => {
         duration={1}
         className="flex flex-col gap-8 md:w-6/12 w-12/12"
       >
-        <p className="text-sm text-[#88888b] tracking-widest" >FREELANCE / FULL-TIME · RIZAL, PH</p>
+        <p className="text-sm text-[#88888b] tracking-widest">
+          FREELANCE / FULL-TIME · RIZAL, PH
+        </p>
         <h1 className="md:text-5xl text-3xl">
           {!revealRole ? (
             "Software Developer"
@@ -46,16 +59,41 @@ const Hero = () => {
         <div className="text-[#a7a9be] md:text-base text-sm">
           <TypeAnimation sequence={[Paragraph, 1000]} speed={99} />
         </div>
-        <div>
+        <div className="flex flex-row md:gap-4 gap-2">
           <motion.button whileHover={{ scale: 1.05 }} whileTap={{ scale: 1 }}>
-            <Button variant="primary" classname="md:w-60 w-40">
+            <Button variant="primary" className="md:w-60 w-40">
               Download Resume
+            </Button>
+          </motion.button>
+          <motion.button whileHover={{ scale: 1.05 }} whileTap={{ scale: 1 }}>
+            <Button
+              variant="outline"
+              className="md:w-60 w-40"
+              onClick={() => {
+                navigator.clipboard.writeText("miranj8157@gmail.com"); // This doesnt work on mobile when on local
+                setCopyEmail(true);
+              }}
+            >
+              {!copyEmail ? (
+                "Copy Email"
+              ) : (
+                <div className="flex flex-row items-center justify-center gap-4">
+                  Copied{" "}
+                  <ion-icon
+                    name="checkmark-done-outline"
+                    class="text-lg"
+                  ></ion-icon>
+                </div>
+              )}
             </Button>
           </motion.button>
         </div>
       </Motion>
       <Motion direction="right" className="">
-        <img src={Profile} className="h-60 w-60 md:w-90 md:h-90 rounded-full"></img>
+        <img
+          src={Profile}
+          className="h-60 w-60 md:w-90 md:h-90 rounded-full"
+        ></img>
       </Motion>
     </div>
   );
--- frontend/src/features/utils/Button.jsx
+++ frontend/src/features/utils/Button.jsx
@@ -1,11 +1,13 @@
-const button = ({ variant = "primary", classname = "", children }) => {
+const button = ({ variant = "primary", className = "", onClick, children }) => {
   const variants = {
     primary: "bg-[#ff8906] hover:opacity-60",
+    outline: "border border-[#ff8906] text-[#ff8906]",
   };
 
   return (
     <button
-      className={`text-center md:text-base text-xs md:py-5 p-3 cursor-pointer ${variants[variant]} ${classname}`}
+      className={`text-center md:text-base text-xs md:py-5 p-3 cursor-pointer ${variants[variant]} ${className}`}
+      onClick={onClick}
     >
       {children}
     </button>
