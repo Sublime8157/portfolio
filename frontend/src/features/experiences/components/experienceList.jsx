7a9236f92f52cfdce9d26f30e0a00f225faa9334
Joven <nevojnarim@gmail.com>
2026-03-26 12:58:49 +0800

    Replace experiences UI

--- frontend/src/features/experiences/components/experienceList.jsx
+++ frontend/src/features/experiences/components/experienceList.jsx
@@ -7,9 +7,10 @@ const experiences = [
     name: "Jeonsoft Corporation",
     role: "Software Developer",
     tenure: "July 2024 - January 2026",
-    paragraph: ` Maintained and enhanced a production payroll system used by enterprise clients, resolving critical payroll computation issues affecting over 500 employees. \n
+    paragraph: ` Maintained and enhanced a production payroll system used by enterprise clients, resolving critical payroll computation issues affecting over 10,000 employees. \n
     Developed and customized payroll features using Node.js, Ruby on Rails, SQL, WebixJS, and Tailwind CSS, improving system usability and overall functionality. \n
     Optimized SQL queries, reducing payroll report generation time by 40%, and collaborated with senior developers to deploy fixes and enhancements to live production environments.`,
+    stacks: ["SQL", "Ruby on Rails", "NodeJS", "WebixJS", "TailwindCSS"],
   },
   {
     logo: ExpLogo2,
@@ -18,6 +19,7 @@ const experiences = [
     tenure: "October 2023 - February 2024",
     paragraph:
       "Automated key workflows and processes for the company website using Go High Level, improving marketing automation, lead capture, and customer engagement. \n \n  Collaborated with the development team to design and build the front-end of the company website, ensuring a responsive and user-friendly interface. \n \n  Participated in meetings with international clients to gather project specifications and requirements, helping translate business needs into technical implementation for the website.",
+      stacks: ["Go High Level","Airtable","Zapier"]
   },
 ];
 
