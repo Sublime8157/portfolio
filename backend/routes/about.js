e2096fe61aeb6e0c9c47d57c3ae41eee1c33d324
Joven <nevojnarim@gmail.com>
2026-03-29 16:29:39 +0800

    Support CRUD for About section

---
+++ backend/routes/about.js
@@ -0,0 +1,67 @@
+import prisma from "../lib/prisma.ts";
+import express from "express";
+
+const router = express.Router();
+
+router.get("/:id", async (req, res) => {
+  try {
+    const about = await prisma.about.findFirst({
+      where: { id: parseInt(req.params.id) },
+    });
+    res.json(about);
+  } catch (error) {
+    console.error("Error fetching about section:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the about section." });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const data = req.body;
+  try {
+    const newAbout = await prisma.about.create({
+      data: data,
+    });
+    res.json(newAbout);
+  } catch (error) {
+    console.error("Error creating about section:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the about section." });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    await prisma.about.delete({
+      where: { id: parseInt(id) },
+    });
+    res.json({ message: "About section deleted successfully." });
+  } catch (error) {
+    console.error("Error deleting about section:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the about section." });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+  try {
+    const updatedData = await prisma.about.update({
+      where: { id: parseInt(id) },
+      data: data,
+    });
+    res.json(updatedData);
+  } catch (error) {
+    console.error("Error updating about section:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while updating the about section." });
+  }
+});
+
+export default router;
