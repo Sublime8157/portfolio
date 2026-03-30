6c6f8013d6b9d243e39e6f014eafcd4bad057123
Joven <nevojnarim@gmail.com>
2026-03-30 11:54:18 +0800

    Support CRUD on experiences section

---
+++ backend/routes/experiences.js
@@ -0,0 +1,81 @@
+import prisma from "../lib/prisma.ts";
+import express from "express";
+
+const router = express.Router();
+
+// Get all experiences
+router.get("/", async (req, res) => {
+  try {
+    const experiences = await prisma.experiences.findMany({
+      include: {
+        stack: true, // Include related experience_stack data
+      },
+    });
+    res.json(experiences);
+  } catch (error) {
+    console.error("Error fetching experiences:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching experiences." });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { company, title, tenure, paragraph } = req.body;
+  try {
+    const newExperience = await prisma.experiences.create({
+      data: {
+        company: company,
+        title: title,
+        tenure: tenure,
+        paragraph: paragraph,
+      },
+    });
+    res.status(201).json(newExperience);
+  } catch (error) {
+    console.error("Error creating experience:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the experience." });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await prisma.experiences.delete({
+      where: { id: parseInt(id) },
+    });
+
+    res.status(200).json({ message: "Experience removed successfully." });
+  } catch (error) {
+    console.error("Error deleting experience:", error);
+    res.status(500).json({
+      error: "An error occurred while deleting the experience.",
+    });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { company, title, tenure, paragraph } = req.body;
+  try {
+    const updatedExperience = await prisma.experiences.update({
+      where: { id: parseInt(id) },
+      data: {
+        company: company,
+        title: title,
+        tenure: tenure,
+        paragraph: paragraph,
+      },
+    });
+    res.status(200).json(updatedExperience);
+  } catch (error) {
+    console.error("Error updating experience:", error);
+    res.status(500).json({
+      error: "An error occurred while updating the experience.",
+    });
+  }
+});
+
+export default router;
